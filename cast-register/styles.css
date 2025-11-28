1af9845d74effdff43d614c2ba82d6d214af087c
Goxi Dev <246674146+Goxi-bit@users.noreply.github.com>
2025-11-28 19:03:38 +0100

    Create styles.css

---
+++ cast-register/styles.css
@@ -0,0 +1,37 @@
+h1 {
+  background: linear-gradient(to bottom, #7ecc00 9%, white 50%, #55cc00 100%);
+  font-size: 2em;
+  text-align: center;
+ 
+   }
+   main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 25px;
+
+    
+}
+
+p {
+  color: white;
+  font-size: 1.5em;
+}
+
+#purchase-btn {
+    padding: 10px 15px;
+    border-radius: 5px;
+    background-color: #af9027;
+    color: white;
+    border: none;
+    cursor: pointer;
+    transition: background 0.3s;
+}
+#change-due {
+  font-size: 1.2em;
+  color: white;
+  padding:15px 15px;
+  margin: 15px;
+  
+}
